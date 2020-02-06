edc778c07ccfc79251806fc326592d647dcbde20
Megat Syafiq <megat.fst@gmail.com>
2020-02-06 10:46:31 +0800

    first commit

---
+++ BookList/BookInfoVC.swift
@@ -0,0 +1,30 @@
+//
+//  BookInfoVC.swift
+//  BookList
+//
+//  Created by Megat Syafiq on 05/02/2020.
+//  Copyright © 2020 Megat Syafiq. All rights reserved.
+//
+
+import UIKit
+
+class BookInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BookList/BookListCVC.swift
@@ -0,0 +1,13 @@
+//
+//  ListBookCVC.swift
+//  BookList
+//
+//  Created by Megat Syafiq on 05/02/2020.
+//  Copyright © 2020 Megat Syafiq. All rights reserved.
+//
+
+import UIKit
+
+class ListBookCVC: UICollectionViewCell {
+    
+}
